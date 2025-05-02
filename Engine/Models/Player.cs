622eea26c06ac461679eba42190f4bfc49d9733d
MEdw4rds <MatthewChrEdwards@gmail.com>
2025-05-02 18:16:37 +0100

    created grid for CurrentPlayer

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -9,9 +9,10 @@ namespace Engine.Models
     public class Player
     {
         public string Name {  get; set; }
-        public string Class { get; set; }
+        public string CharacterClass { get; set; }
         public int HitPoints { get; set; }
         public int ExperiencePoints { get; set; }
         public int Gold { get; set; }
+        public int Level { get; set; }
     }
 }

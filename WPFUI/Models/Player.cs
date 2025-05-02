41978316496b6c1180141b46dcc37f96b42e4bd2
MEdw4rds <MatthewChrEdwards@gmail.com>
2025-05-02 16:57:03 +0100

    create player model, and gamesession viewmodel

---
+++ WPFUI/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.Models
+{
+    public class Player
+    {
+        public string Name {  get; set; }
+        public string Class { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ WPFUI/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFUI.Models;
+
+namespace WPFUI.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer {  get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scot";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}

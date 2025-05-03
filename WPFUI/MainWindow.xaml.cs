e9c1539c6eb3c3aaa6718403ef214728e08d5026
MEdw4rds <MatthewChrEdwards@gmail.com>
2025-05-03 14:22:54 +0100

    Add location class and images, change engine solutiion to sdk windows desktop to fix image referencing.

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -18,14 +18,15 @@ namespace WPFUI
     public partial class MainWindow : Window
     {
         private GameSession _gameSession;
-
         public MainWindow()
         {
             InitializeComponent();
-            
             _gameSession = new GameSession();
-
             DataContext = _gameSession;
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
         }
     }
 }
